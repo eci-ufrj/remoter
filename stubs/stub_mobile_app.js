e1afdb73a763d6b0514a0ac93c22e2390c52fd7c
Yuri <yuri.vasquez91@gmail.com>
2014-04-21 21:48:23 -0300

    Full project reorganization
    - Removal of "server" folder
    - Creation of "stub" folder and replacement of templates folder from "server"
    - app_server.js to stub_web_app.js rename
    - stub_web_app.js rework
    - stub_mobile_app.js creation
    - Added bower support to stubs
    - templates/mobile created
    
    -TODO: README.md update

---
+++ stubs/stub_mobile_app.js
@@ -0,0 +1,12 @@
+//Run locally, serving static files or whatever, for testing purposes
+var express = require('express');
+var app = express();
+
+app.use("/components",express.static(__dirname+"/bower_components"));
+app.use("/js",express.static(__dirname+"/static/js"));
+app.use("/",express.static(__dirname+"/templates/mobile/"))
+
+
+
+
+app.listen(4000);
